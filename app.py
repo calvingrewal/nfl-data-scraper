c432646377446fd34da72b6c255fa2ce2993fe82
Calvin Grewal <calvingrewal28@gmail.com>
2017-09-07 13:39:26 -0700

    scrapes game data from table for all teams from 2000-2016

--- app.py
+++ app.py
@@ -1,23 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-weeks = []
-for i in range(2000, 2017):
 
-    url = "https://www.pro-football-reference.com/teams/nwe/%s.htm" % (i)
-    page = requests.get(url)
-    contents = page.content
+afcTeams = ['nwe', 'mia', 'nyj', 'buf', 'rav', 'pit', 'cle', 'cin', 'clt', 'jax', 'htx', 'oti', 'sdg', 'rai', 'kan', 'den']
 
-    soup = BeautifulSoup(contents, 'html.parser')
-    table = soup.select('#all_games table')
-    rows = table[0].find('tbody').find_all('tr')
+nfcTeams = ['dal', 'nyg', 'was', 'phi', 'gnb', 'det', 'min', 'chi', 'atl', 'tam', 'nor', 'car', 'sea', 'crd', 'ram', 'sfo']
 
-    for row in rows:
-        week = []
-        for stat in row:
-            week.append(stat.text)
-        weeks.append(week)
+for team in nfcTeams:
+    weeks = []
+    for i in range(2000, 2017):
+        print('%s %s' %(team, i))
+        url = "https://www.pro-football-reference.com/teams/%s/%s.htm" % (team, i)
+        page = requests.get(url)
+        contents = page.content
 
-with open('nwe.csv', 'w') as f:
-    csvfile = csv.writer(f, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    csvfile.writerows(weeks)
+        soup = BeautifulSoup(contents, 'html.parser')
+        table = soup.select('#all_games table')
+        if len(table) > 0:
+            rows = table[0].find('tbody').find_all('tr')
+
+            for row in rows:
+                week = []
+                for stat in row:
+                    week.append(stat.text)
+                weeks.append(week)
+
+
+    with open(team+'.csv', 'w') as f:
+        csvfile = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csvfile.writerows(weeks)
